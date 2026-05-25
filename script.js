03bc817fedc5e8b77b0a1ef2be6f0924f612ef1a
João Rafael Engelke Santos <156162459+Engelkejr@users.noreply.github.com>
2026-05-25 13:34:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+        function copyEmail(e) {
+            e.preventDefault();
+            var copyText = document.getElementById("hidden-email");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999); 
+            
+            var toast = document.getElementById("toast");
+            toast.className = "show";
+            setTimeout(function(){ toast.className = toast.className.replace("show", ""); }, 3000);
+        }
